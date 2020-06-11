544cc835389cf2d556c06d4efc11fe8231e9feb9
Arif Luthfiansyah <lutluthfi@icloud.com>
2020-06-11 15:08:06 +0700

    Initial commit

---
+++ MysteryMessage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MysteryMessage
+//
+//  Created by Arif Luthfiansyah on 11/06/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

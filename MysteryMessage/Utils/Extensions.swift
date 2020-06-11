5bffbd3c282f4e7501a35e030987f308734d28ce
Muhammad Nobel Shidqi <mnobelshidqi@gmail.com>
2020-06-11 20:34:14 +0700

    Game Scene Features

---
+++ MysteryMessage/Utils/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  MysteryMessage
+//
+//  Created by Muhammad Nobel Shidqi on 11/06/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import Foundation
---
+++ MysteryMessage/Views/Collection Views/MessageCell.swift
@@ -0,0 +1,21 @@
+//
+//  MessageCell.swift
+//  MysteryMessage
+//
+//  Created by Muhammad Nobel Shidqi on 11/06/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
